97414b35d7dada72f514b198ff325804c90a729c
timothylai53 <timothylai53@gmail.com>
2026-04-18 15:11:38 +0800

    Add SendGrid

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+import 'dotenv/config';
+
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
